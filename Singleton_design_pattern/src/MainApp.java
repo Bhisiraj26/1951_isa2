d585f588d9ee287aff12c4e78b5dfcd1f61876ec
Bhisiraj26 <mca.1951@unigoa.ac.in>
2021-04-28 20:02:18 +0530

    second commit

---
+++ Singleton_design_pattern/src/MainApp.java
@@ -0,0 +1,9 @@
+public class MainApp {
+
+	public static void main( String[] args )
+	  {
+	    Singleton.getSingletonInstance().printSingleton();
+	    Singleton.getSingletonInstance().printSingleton();
+	    Singleton.getSingletonInstance().printSingleton();    
+	  }
+}
---
+++ Singleton_design_pattern/src/Singleton.java
@@ -0,0 +1,27 @@
+public class Singleton
+{
+  private static Singleton singletonInstance;
+
+  private Singleton()
+  {
+    
+  }
+
+  public static Singleton getSingletonInstance()
+  {
+    if( null == singletonInstance )
+    {
+    	singletonInstance = new Singleton();
+    	System.out.println("Inside null check , Object has been Created : "+singletonInstance.toString());
+    	System.out.println(" ******************************* ");
+    }
+    return singletonInstance;
+  }
+
+  public void printSingleton()
+  {
+	  System.out.println("Inside print Singleton object : " +singletonInstance.toString());
+	  System.out.println("");
+  }
+
+}
